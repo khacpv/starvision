cf1b2728b22a62efb11c327f4b59d3de9043f61f
starvision <starvision.oicsoft.com@gmail.com>
2020-03-15 14:09:51 +0700

    update code

--- backend/deploy-win.bat
+++ backend/deploy-win.bat
@@ -6,3 +6,4 @@ call npm run pm2 stop starvision.oicsoft.com
 call npm run pm2 delete starvision.oicsoft.com
 call npm run pm2 start ./bin/www -- --name starvision.oicsoft.com -i 4 -l C:/website/starvision.oicsoft.com/logs/logs.txt
 call npm run pm2 list
+pause
