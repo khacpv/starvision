30d7e62a3fcd2e67b470a31ebd2c441144b82ef4
slade77777 <dangxuancuong354@gmail.com>
2020-03-16 23:04:54 +0700

    setup env

--- frontend/webpack.config.js
+++
@@ -1,7 +0,0 @@
-process.env.NODE_ENV = 'production';
-var reactScriptsConfig = require('react-scripts/config/webpack.config.prod');
-module.exports = Object.assign({}, reactScriptsConfig, {
-  output: Object.assign({}, reactScriptsConfig.output, {
-    path: '../public'
-  })
-});
