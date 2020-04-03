eba6c025c0c895b041c6cf74de9138c80c6fa98b
starvision <starvision.oicsoft.com@gmail.com>
2020-04-03 23:24:37 +0700

    fix error build

--- backend/deploy-dev.bat
+++ backend/deploy-dev.bat
@@ -1,12 +1,5 @@
-rem git pull origin master
-
-set NODE_ENV="development"
+set NODE_ENV=production
 set PORT=3001
-rem cd ../frontend
-rem call npm run build
-rem cd ../backend
-rem call npm run pm2 stop starvision-dev.oicsoft.com
-call npm run pm2 delete starvision-dev.oicsoft.com
-call npm run pm2 start ./bin/www -- --name starvision-dev.oicsoft.com -i 2 -l C:/website/starvision-dev.oicsoft.com/logs/logs.txt
-rem call npm run pm2 list
+rem npm run pm2 delete starvision-dev.oicsoft.com
+npm run pm2 start ./bin/www -- --name starvision-dev.oicsoft.com -i 1 -l C:/website/starvision-dev.oicsoft.com/logs/logs.txt
 pause
