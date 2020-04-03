6b3545487d5020cf9005017c4e106d32e4adcaa7
starvision <starvision.oicsoft.com@gmail.com>
2020-04-03 23:27:07 +0700

    update deploy code

--- backend/deploy-dev.bat
+++ backend/deploy-dev.bat
@@ -1,5 +1,6 @@
 set NODE_ENV=production
 set PORT=3001
 rem npm run pm2 delete starvision-dev.oicsoft.com
-npm run pm2 start ./bin/www -- --name starvision-dev.oicsoft.com -i 1 -l C:/website/starvision-dev.oicsoft.com/logs/logs.txt
+rem npm run pm2 start ./bin/www -- --name starvision-dev.oicsoft.com -i 1 -l C:/website/starvision-dev.oicsoft.com/logs/logs.txt
+npm run pm2 reload starvision-dev.oicsoft.com
 pause
