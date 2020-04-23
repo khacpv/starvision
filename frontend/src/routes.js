f9afdcb832d4b062f95f4f9e582d373c4323e3f5
slade77777 <dangxuancuong354@gmail.com>
2020-04-23 22:07:00 +0700

    follow up

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
 const Dashboard = React.lazy(() => import('./views/Dashboard'));
-const FollowUp = React.lazy(() => import('./views/FollowUp'));
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
-  { path: '/follow-up', name: 'FollowUp', component: FollowUp },
 ];
 
 export default routes;
