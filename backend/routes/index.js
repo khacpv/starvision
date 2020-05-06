fbe554dea3d153567f1de2547f50d008fc73f271
Bac Vu <vuvan.chanqua@gmail.com>
2020-05-06 22:45:46 +0700

    Update customer ok type

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,7 +4,6 @@ const CustomerController = require("../controllers/customer_controller");
 const UserController = require("../controllers/user_controller");
 const CustomerOkController = require("../controllers/customerok_controller");
 const FittingController = require("../controllers/fitting_controller");
-const CongnoController = require("../controllers/congno_controller");
 //__INIT_CONTROLLER__
 const NotificationsController = require("../controllers/notifications_controller");
 const OrderlenseController = require("../controllers/orderlense_controller");
