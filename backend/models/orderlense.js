d58305ecc7c51ca30279f4aef4c65d468aad1247
Bac vu <vuvan.chanqua@gmail.com>
2021-04-06 23:25:44 +0700

    update backend

--- backend/models/orderlense.js
+++ backend/models/orderlense.js
@@ -1,22 +1,37 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const OrderLense = sequelize.define('OrderLense', {
-    doctor_code: DataTypes.STRING,
-    doctor_id: DataTypes.STRING,
-    customer_id: DataTypes.STRING,
-    dttc_id: DataTypes.STRING,
-    date_examination: DataTypes.STRING,
-    id_lense_left: DataTypes.INTEGER,
-    id_lense_right: DataTypes.INTEGER,
-    order_number: DataTypes.STRING,
-    is_active: DataTypes.INTEGER,
-    note: DataTypes.TEXT,
-    type: DataTypes.STRING
-  }, {});
-  OrderLense.associate = function(models) {
+  const OrderLense = sequelize.define(
+    "OrderLense",
+    {
+      doctor_code: DataTypes.STRING,
+      doctor_id: DataTypes.STRING,
+      customer_id: DataTypes.STRING,
+      dttc_id: DataTypes.STRING,
+      date_examination: DataTypes.STRING,
+      id_lense_left: DataTypes.INTEGER,
+      id_lense_right: DataTypes.INTEGER,
+      order_number: DataTypes.STRING,
+      is_active: DataTypes.INTEGER,
+      note: DataTypes.TEXT,
+      type: DataTypes.STRING,
+    },
+    {}
+  );
+  OrderLense.associate = function (models) {
     // associations can be defined here
-    OrderLense.belongsTo(models.Lense, { as: 'left', foreignKey: 'id_lense_left' });
-    OrderLense.belongsTo(models.Lense, { as: 'right', foreignKey: 'id_lense_right' });
+    OrderLense.belongsTo(models.Lense, {
+      as: "left",
+      foreignKey: "id_lense_left",
+    });
+    OrderLense.belongsTo(models.Lense, {
+      as: "right",
+      foreignKey: "id_lense_right",
+    });
+    OrderLense.belongsTo(models.Doctors, {
+      as: "customer",
+      foreignKey: "customer_id",
+      targetKey: "id",
+    });
   };
   return OrderLense;
 };
